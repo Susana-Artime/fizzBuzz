5f98df0ad9a1c46953e3702eb18d8f448d627c9c
SusanaArtime <susartime@gmail.com>
2024-10-15 21:30:59 +0200

    subir archivos finales

--- src/main/java/org/factoriaf5/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/factoriaf5/fizzbuzz/FizzBuzz.java
@@ -2,8 +2,25 @@ package org.factoriaf5.fizzbuzz;
 
 public class FizzBuzz {
 
+       public String devolverNumero(int i){
+
+          String resultado = "";
+
+             if (i % 3 == 0 || String.valueOf(i).contains("3")) {
+                resultado += "Fizz";
+            }
+            
+                    
+            if (i % 5 == 0 || String.valueOf(i).contains("5")) {
+                resultado += "Buzz";
+            }
+            
+             return resultado.isEmpty() ? String.valueOf(i) : resultado;
+        }
+             
+
+    }
 
 
-    
-    
-}
+
+
--- src/test/java/org/factoriaf5/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/org/factoriaf5/fizzbuzz/FizzBuzzTest.java
@@ -1,5 +1,45 @@
 package org.factoriaf5.fizzbuzz;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
+
+    @Test
+    public void devolverNumeroTest(){
+
+        FizzBuzz fizzBuzz=new FizzBuzz();
+
+        assertEquals("Fizz", fizzBuzz.devolverNumero(3));
+        assertEquals("Fizz", fizzBuzz.devolverNumero(6));
+        assertEquals("Fizz", fizzBuzz.devolverNumero(9));
+        assertEquals("Fizz", fizzBuzz.devolverNumero(123));
+        
+        
+        assertEquals("Buzz", fizzBuzz.devolverNumero(5));
+        assertEquals("Buzz", fizzBuzz.devolverNumero(10));
+        assertEquals("Buzz", fizzBuzz.devolverNumero(25));
+
+
+        assertEquals("FizzBuzz", fizzBuzz.devolverNumero(15));
+        assertEquals("FizzBuzz", fizzBuzz.devolverNumero(30));
+        assertEquals("FizzBuzz", fizzBuzz.devolverNumero(135));
+        assertEquals("FizzBuzz", fizzBuzz.devolverNumero(53));
+
+        
+        assertEquals("1", fizzBuzz.devolverNumero(1));
+        assertEquals("2", fizzBuzz.devolverNumero(2));
+        assertEquals("4", fizzBuzz.devolverNumero(4));
+        assertEquals("7", fizzBuzz.devolverNumero(7));
+        assertEquals("8", fizzBuzz.devolverNumero(8));
+
+
+
+
+
+    }
+
+
+
     
 }
